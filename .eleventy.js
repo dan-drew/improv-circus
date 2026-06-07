c66bc40faf53efaa59af5df771932eaa7ba3beaa
Dan Drew <dan.drew@hotmail.com>
2026-06-07 11:41:34 -0700

    github pages fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
 import pugPlugin from '@11ty/eleventy-plugin-pug';
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 import YAML from "yaml";
+
+// function pagePrefix() {
+//   let value = process.envPAGE_PREFIX
+//   if (value && value.length > 0) {
+//     return `/${value}/`.replace('//', '/')
+//   }
+//   return '/'
+// }
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(pugPlugin);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.setTemplateFormats('pug,md');
   eleventyConfig.setInputDirectory('src');
+  // eleventyConfig.setPathPrefix(pagePrefix());
   eleventyConfig.addPassthroughCopy('src/css')
   eleventyConfig.addPassthroughCopy('src/images')
   eleventyConfig.addPassthroughCopy('src/js')
